9edc1d38061ee855eae8956f10be0249bd8d5967
Leticia Mazzo <leticiamazzo@gmail.com>
2022-08-01 12:05:56 -0300

    trainning arrow functions and template strings

---
+++ javascript-modern-es6/modern-js.js
@@ -0,0 +1,17 @@
+// const letters = ['a', 'b', 'c', 'd', 'e']
+
+// const alphabet1 = letters.map(function(element){
+//     return `${element} é uma letra`
+// })
+// console.log(alphabet1);
+
+// const alphabet2 = letters.map(element => `${element} é uma letra`)
+// console.log(alphabet2);
+
+
+// const chain = letters
+//     .filter(element => element === 'b')
+//     .map(element => `${element} é a letra que filtrei`);
+
+// console.log(chain);
+
